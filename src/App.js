85c0f0cf867248b3dedf0974eb339c8f1321e475
dellta103 <dellta103@gmail.com>
2022-03-08 14:19:52 +0700

    new

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Test from "./components";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <Test />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Test() {
+  return (
+    <div className="p-5 rounded-3xl container max-w-lg bg-black min-h-screen">
+      <h1 className="display-4 font-semibold text-center ">Test</h1>
+    </div>
+  );
+}

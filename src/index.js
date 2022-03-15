7d558d4770fa884819099446fedba17a53f0a5fc
dellta103 <dellta103@gmail.com>
2022-03-15 19:28:36 +0700

    update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
-import { rootReducer } from "./components/reducer/rootReducer";
+import { rootReducer } from "./redux/reducer/rootReducer";
 
 let store = createStore(rootReducer);
 
